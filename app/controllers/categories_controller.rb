be11436db915c3c66980f9619c4d0334077e8d3d
Sohaib Hassan <sohaib@elgrocer.com>
2025-09-08 20:32:05 +0500

    Category Controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :destroy]
+  def index
+    @categories = Category.all
+    render json: @categories
+  end
+  def show
+    render json: @category, include: :products
+  end
+  def create
+  @category = Category.new(category_params)
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+  def update
+    if @category.update(category_params)
+      render json: @category, status: :ok
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @category.destroy
+    head :no_content
+  end
+  private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
